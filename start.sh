16281f103da756d546ebe88275b3e50dff53a9f8
jack0sparrow1 <tbhanu360@gmail.com>
2025-04-14 14:07:05 +0530

    Initial commit

--- start.sh
+++ start.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-apt-get update && apt-get install -y \
-    libgstreamer1.0-0 \
-    gstreamer1.0-plugins-base \
-    gstreamer1.0-plugins-good \
-    gstreamer1.0-plugins-bad \
-    gstreamer1.0-plugins-ugly \
-    gstreamer1.0-libav \
-    libgstreamer-plugins-base1.0-dev \
-    libsdl2-dev \
-    libsdl2-mixer-dev
-
+# start.sh backup script
 python bot.py
